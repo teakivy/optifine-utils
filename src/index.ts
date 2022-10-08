ba146bf7e9f2bd56994c3c86ab9a217902160f46
teakivy <legocjones@gmail.com>
2022-10-08 18:43:11 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+const url = 'https://optifine.net/adloadx?f='; // URL
+const baseUrl = 'https://optifine.net/'; // Base URL
+
+const AxiosInstance = axios.create(); // Create a new Axios Instance
+
+export const getDownloadLink = async (fileName: string) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const { data } = await AxiosInstance.get(url + fileName); // Get the HTML from the URL
+            const $ = cheerio.load(data); // Load the HTML into Cheerio
+            const downloadLink = $('#Download > a').attr('href'); // Get the download link from the HTML
+            resolve(baseUrl + downloadLink); // Resolve the Promise with the download link
+        } catch (error) {
+            reject(error); // Reject the Promise with the error
+        }
+    });
+};
+
+async function main() {
+    await getDownloadLink('OptiFine_1.19.2_HD_U_H9.jar').then(
+        (downloadLink) => {
+            console.log(downloadLink); // Log the download link
+        }
+    );
+}
+
+main();
